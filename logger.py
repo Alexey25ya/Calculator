bd6581112474f7ee22a4fb11781827690930d3c8
Lena <len.dal@yandex.ru>
2022-09-28 16:05:48 +0300

    deleted logger, scheme

--- logger.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime as dt
-
-def logger(data, result):
-    time = dt.now().strftime('%m.%d.%Y - %H:%M')
-    with open('log.csv', 'a') as file:
-        file.write(f'{data}: {time} = {result}\n')
